09aaa4a71a659934ef030159540e866b499f8d3f
J. Michael Burgess <jmichaelburgess@gmail.com>
2019-04-26 16:31:58 +0200

    finished up effective area

--- pyipn/effective_area.py
+++ pyipn/effective_area.py
@@ -2,17 +2,69 @@
 class EffectiveArea(object):
 
     def __init__(self, total_area):
+        """FIXME! briefly describe function
+
+        :param total_area: 
+        :returns: 
+        :rtype: 
+
+        """
 
         self._total_area = area
-
+        self._seperation_angle = None
+        
+        
     @property
     def total_area(self):
         return self._total_area
 
 
-    def _transform(self):
-        pass
+    @property
+    def effective_area(self):
+        """FIXME! briefly describe function
 
-    def effective_area_at(self, ra, dec):
+        :returns: 
+        :rtype: 
 
-        pass
+        """
+
+        if self._seperation_angle is not None:
+
+            return self.effective_area_at(self._seperation_angle)
+
+        else:
+
+            return self._total_area
+        
+        
+    def set_seperation_angle(self, angle):
+        """FIXME! briefly describe function
+
+        :param angle: 
+        :returns: 
+        :rtype: 
+
+        """
+
+        self._seperation_angle = angle
+
+        
+    def effective_area_at(self, angle):
+        """FIXME! briefly describe function
+
+        :param angle: 
+        :returns: 
+        :rtype: 
+
+        """
+
+        if angle >= 0.5 * np.pi:
+
+            return 0.
+
+        else:
+
+            return self._total_area * np.cos(angle)
+
+
+    
