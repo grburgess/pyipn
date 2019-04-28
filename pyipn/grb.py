453770f81f1ce8287300ed421b347e1bfdd2d6fe
J. Michael Burgess <jmichaelburgess@gmail.com>
2019-04-28 20:24:30 +0200

    updated with docs

--- pyipn/grb.py
+++ pyipn/grb.py
@@ -1,24 +1,43 @@
 import numpy as np
 
-from .geometry import Location
+from .geometry import GRBLocation
+
 
 class GRB(object):
+    def __init__(self, ra, dec, distance, K, t_rise, t_decay):
+        """
 
-    def __init__(self, location, K, t_rise, t_decay):
+        A GRB that emits a spectrum as a given location
 
-        self._location = location
+        :param ra: RA of the GRB
+        :param dec: DEC of the GRB
+        :param distance: distance to the GRB
+        :param K: normalization of the flux
+        :param t_rise: rise time of the flux
+        :param t_decay: decay time of the flux
+        :returns: 
+        :rtype: 
+
+        """
+
+        # create a GRB location
+        
+        self._location = GRBLocation(ra, dec, distance)
 
         self._K = K
-        self._t_rise  = t_rise
+        self._t_rise = t_rise
         self._t_decay = t_decay
-
 
     @property
     def pulse_parameters(self):
+        """
+        The temporal flux parameters
+        :returns: (K, t_rise, t_decay) 
+        :rtype: tuple
+
+        """
 
         return self._K, self._t_rise, self._t_decay
-        
-
 
     @property
     def location(self):
