440f3dd7abb6b7578c25d4f4c49710d489c87241
J. Michael Burgess <jmichaelburgess@gmail.com>
2019-04-30 10:26:49 +0200

    update init

--- pyipn/__init__.py
+++ pyipn/__init__.py
@@ -5,3 +5,8 @@
 __author__ = """J. Michael Burgess"""
 __email__ = 'jburgess@mpe.mpg.de'
 __version__ = '0.1.0'
+
+
+from .universe import Universe
+from .io.package_utils import copy_template
+ __all__ = ['Universe', 'copy_template']
