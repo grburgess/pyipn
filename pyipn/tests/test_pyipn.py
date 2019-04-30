51e9aa809018572d0ee2c3c71cb65cb03c29b883
J. Michael Burgess <jmichaelburgess@gmail.com>
2019-04-30 13:37:42 +0200

    typos

--- pyipn/tests/test_pyipn.py
+++ pyipn/tests/test_pyipn.py
@@ -6,20 +6,20 @@
 import pytest
 
 
-from pyipn import pyipn
+# from pyipn import pyipn
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+# @pytest.fixture
+# def response():
+#     """Sample pytest fixture.
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+#     See more at: http://doc.pytest.org/en/latest/fixture.html
+#     """
+#     # import requests
+#     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-g    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+# def test_content(response):
+#     """Sample pytest test function with the pytest fixture as an argument."""
+#     # from bs4 import BeautifulSoup
+# g    # assert 'GitHub' in BeautifulSoup(response.content).title.string
