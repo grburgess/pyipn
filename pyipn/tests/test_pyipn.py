bb1f9cb67270b2e04f5789b9867738683dccdffe
J. Michael Burgess <jmichaelburgess@gmail.com>
2019-04-30 13:17:49 +0200

    added simple test

--- pyipn/tests/test_pyipn.py
+++ pyipn/tests/test_pyipn.py
@@ -22,4 +22,4 @@ def response():
 def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+g    # assert 'GitHub' in BeautifulSoup(response.content).title.string
