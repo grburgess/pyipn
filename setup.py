a34c867f424fcf3db8f0e43750f66f5dbe23c499
grburgess <jmichaelburgess@gmail.com>
2019-05-10 15:36:37 +0200

    add find data files

--- setup.py
+++ setup.py
@@ -75,6 +75,20 @@ REQUIRED = [
 
 
 
+# Create list of data files
+def find_data_files(directory):
+
+    paths = []
+
+    for (path, directories, filenames) in os.walk(directory):
+
+        for filename in filenames:
+
+            paths.append(os.path.join("..", path, filename))
+
+    return paths
+
+
 
 extra_files = find_data_files("pyipn/data")
 
