cdebd912636ce8b26a6400692fe2cea346f4cfa5
msinghartinger <msinghart@mpe.mpg.de>
2019-10-14 19:11:06 +0200

    fermi.py can send queries to spacetrack and get TLEs for specific satellite and timerange

--- pyipn/io/orbits/fermi.py
+++ pyipn/io/orbits/fermi.py
@@ -1,1 +1,14 @@
+#package to calculate satellite positions based on TLE  
 from skyfield.api import Topos, load
+
+#package to send query requests to space-track.org
+from spacetrack import SpaceTrackClient
+st = SpaceTrackClient('singhartinger.moritz@gmail.com', 'WhydoIneedsuchalongpassword')
+
+import spacetrack.operators as op
+import datetime as dt
+drange = op.inclusive_range(dt.datetime(2018, 1, 1),
+                            dt.datetime(2018, 1, 31))
+
+print(st.tle(norad_cat_id=33053, epoch=drange, format='tle'))
+
