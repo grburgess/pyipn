ee52533b2810080cb81433f0b050ca24b7a58a9a
grburgess <jmichaelburgess@gmail.com>
2020-07-09 07:03:49 +0200

    update setup and clean up

--- pyipn/__init__.py
+++ pyipn/__init__.py
@@ -2,12 +2,12 @@
 
 """Top-level package for pyipn."""
 
-__author__ = """J. Michael Burgess"""
-__email__ = "jburgess@mpe.mpg.de"
-__version__ = "0.1.0"
-
 
 from .universe import Universe
 from .io.package_utils import copy_template
 
 __all__ = ["Universe", "copy_template"]
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
