815df2fd18ea3a66dc802c3c7601d5e98c1e6a30
grburgess <jmichaelburgess@gmail.com>
2020-07-24 14:33:05 +0200

    fix up classes

--- pyipn/fit.py
+++ pyipn/fit.py
@@ -1,6 +1,9 @@
 import arviz as av
 import numpy as np
 import numba as nb
+import matplotlib.pyplot as plt
+
+
 from pyipn.rff import RFF
 
 
@@ -20,15 +23,21 @@ class Fit(object):
 
         self._omega2 = inference_data.posterior.omega.stack(
             sample=("chain", "draw")
-        ).values[0]
+        ).values[1]
 
         self._amplitude = inference_data.posterior.amplitude.stack(
             sample=("chain", "draw")
         ).values
-        self._background = inference_data.posterior.background.stack(
+
+        self._background = inference_data.posterior.bkg.stack(
             sample=("chain", "draw")
         ).values
 
+        self._scale = inference_data.posterior.scale.stack(
+            sample=("chain", "draw")
+        ).values
+
+        
         try:
 
             self._dt = inference_data.posterior.dt.stack(
@@ -44,6 +53,9 @@ class Fit(object):
 
             self._is_dt_fit = True
 
+            self._n_dets = self._background.shape[0]
+
+            
         except:
 
             self._is_dt_fit = False
@@ -52,8 +64,22 @@ class Fit(object):
             self._grb_theta = None
             self._grb_phi = None
 
+            self._n_dets = 1
+            
         self._n_samples = self._beta1.shape[-1]
 
+
+        try:
+
+            self._bw = inference_data.posterior.bw.stack( sample=("chain", "draw") ).values
+
+        except:
+
+            self._bw = inference_data.posterior.bw_out.stack( sample=("chain", "draw") ).values
+            
+        
+        self._has_universe = False
+        
     def _detector_check(self, det_number):
 
         assert det_number in range(self._n_dets)
@@ -75,30 +101,47 @@ class Fit(object):
     def set_universe(self, universe):
 
         self._universe = universe
+        self._has_universe = True
 
-    def expected_rate(self, time, detector):
+        
+    def expected_rate(self, time, detector, use_bw=False):
 
         self._detector_check(detector)
 
-        if self._is_dt_fit:
-
-            if detector > 0:
-
-                dt = self._dt[detector]
-
+        if self._is_dt_fit and ( detector > 0):
+            
+            dt = self._dt[detector-1]
+            
         else:
 
             dt = np.zeros(self._n_samples)
 
+
+        if not self._is_dt_fit:
+            
+            amp = self._amplitude
+
+        else:
+
+            amp = self._amplitude[detector]
+
+        if use_bw:
+
+            bw = self._bw
+
+        else:
+
+            bw = np.ones(self._n_samples)
+            
         out = _expeced_rate(
             time,
             self._omega1,
             self._omega2,
             self._beta1,
             self._beta2,
-            self._bw,
+            bw,
             self._scale,
-            self._amplitude,
+            self._amplitude[detector],
             dt,
             self._n_samples,
         )
@@ -108,12 +151,12 @@ class Fit(object):
     def plot_location_fit(self):
         pass
 
-    def plot_light_curve_fit(self, detector,tstart, tstop, dt=.2):
+    def plot_light_curve_fit(self, detector,tstart, tstop, dt=.2, use_bw=False):
 
         self._detector_check(detector)
         assert self._has_universe
 
-        lc = self._universe.lightcurves[list(self._lightcurves.keys())[detector]]
+        lc = self._universe.light_curves[list(self._universe.light_curves.keys())[detector]]
 
         rate, edges, counts = lc.get_binned_light_curve(tstart, tstop, dt)
 
@@ -127,11 +170,19 @@ class Fit(object):
         ax.scatter(mid_points, rate,fc="none", ec="k")
 
 
-        pred_rate = self.expected_rate(mid_points, time)
+        pred_rate = self.expected_rate(mid_points, detector, use_bw)
 
+        if self._is_dt_fit:
+
+            bkg = self._background[detector]
+
+        else:
+
+            bkg = self._background
+        
         for i in range(self._n_samples):
 
-            ax.plot(mid_points, pred_rate[i] + self._background[i])
+            ax.plot(mid_points, pred_rate[i] + bkg[i], color="r", alpha=0.05)
         
 
         return fig
@@ -144,7 +195,7 @@ class Fit(object):
     @property
     def beta2(self):
 
-        return self._beta1
+        return self._beta2
 
     @property
     def omega1(self):
@@ -154,8 +205,17 @@ class Fit(object):
     @property
     def omega2(self):
 
-        return self._omega1
+        return self._omega2
 
+    @property
+    def bw(self):
+
+        return self._bw
+
+    @property
+    def scale(self):
+        return self._scale
+    
     @property
     def dt(self):
 
@@ -171,6 +231,16 @@ class Fit(object):
 
         return self._grb_phi
 
+    @property
+    def amplitude(self):
+
+        return self._amplitude
+
+    @property
+    def background(self):
+
+        return self._background
+    
 
 @nb.njit()
 def _expeced_rate(time, omega1, omega2, beta1, beta2, bw, scale, amplitude, dt, N):
@@ -180,7 +250,7 @@ def _expeced_rate(time, omega1, omega2, beta1, beta2, bw, scale, amplitude, dt, 
     for n in range(N):
 
         out[n] = amplitude[n] * RFF(
-            time - dt[n], omega1[n], omega2[n], beta1[n], beta2[n], bw[n], scale[n]
+            time - dt[n], omega1[:,n], omega2[:,n], beta1[:,n], beta2[:,n], bw[n], scale[n]
         )
 
     return out
--- pyipn/rff.py
+++ pyipn/rff.py
@@ -1,14 +1,14 @@
 import numpy as np
 import numba as nb
 
-@njit(fastmath=True)
+@nb.njit(fastmath=True)
 def RFF(time, omega1, omega2, beta1, beta2, bw, scale):
 
 
         features1 = bw * np.outer(time, omega1)
         features2 = bw * np.outer(time, omega2)
 
-        fhat = np.exp( scale * (np.dot( np.cos(features1) + np.cos(features2), beta1) + np.dot(np.sin(features1) + np.sin(features2), beta2)))
+        log_fhat = scale * (np.dot( np.cos(features1) + np.cos(features2), beta1) + np.dot(np.sin(features1) + np.sin(features2), beta2))
 
 
-        return fhat
+        return np.exp(log_fhat)
